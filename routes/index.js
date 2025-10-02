e879659ff884f8b2ab1a076455baa2d5eed8de49
udodavid <davidudo00@gmail.com>
2025-10-02 12:56:50 +0100

    installed swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
+    //swagger.tags=['Hello World, This is Udo David']
     res.send('Hello World, This is Udo David');
 });
 
